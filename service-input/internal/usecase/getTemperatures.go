a8c9e63a567866ce201c0510868996026cd3b598
Victor Kamei Kay <kameikay@gmail.com>
2024-03-02 13:01:39 -0300

    [service-input] add get temperature use case

---
+++ service-input/internal/usecase/getTemperatures.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"context"
+
+	"github.com/kameikay/service-input/internal/service"
+)
+
+type GetTemperaturesUseCase struct {
+	weatherApiService service.GetTemperatureServiceInterface
+}
+
+type Response struct {
+	City  string  `json:"city"`
+	TempC float64 `json:"temp_C"`
+	TempF float64 `json:"temp_F"`
+	TempK float64 `json:"temp_K"`
+}
+
+func NewGetTemperatureUseCase(weatherApiService service.GetTemperatureServiceInterface) *GetTemperaturesUseCase {
+	return &GetTemperaturesUseCase{
+		weatherApiService: weatherApiService,
+	}
+}
+
+func (u *GetTemperaturesUseCase) Execute(ctx context.Context, cep string) (Response, error) {
+	weatherData, err := u.weatherApiService.GetTemperatureService(ctx, cep)
+	if err != nil {
+		return Response{}, err
+	}
+
+	return Response{
+		City:  weatherData.City,
+		TempC: weatherData.TempC,
+		TempF: weatherData.TempF,
+		TempK: weatherData.TempK,
+	}, nil
+
+}
