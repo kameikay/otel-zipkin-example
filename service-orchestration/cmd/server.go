0e0ffeeeef6545a7136ac6f9b2f39c6f07f27972
Victor Kamei Kay <kameikay@gmail.com>
2024-03-05 19:45:26 -0300

    [service-orchestration]: add otel init and graceful shutdown on main

--- service-orchestration/cmd/server.go
+++ service-orchestration/cmd/server.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/kameikay/service-orchestration/configs"
 	"github.com/kameikay/service-orchestration/internal/infra/web/controllers"
 	"github.com/kameikay/service-orchestration/internal/infra/web/handlers"
@@ -14,6 +20,23 @@ func main() {
 		panic(err)
 	}
 
+	signChannel := make(chan os.Signal, 1)
+	signal.Notify(signChannel, os.Interrupt)
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	shutdown, err := configs.SetupOTel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := shutdown(ctx); err != nil {
+			log.Fatal("failed to shutdown tracer provider: ", err)
+		}
+	}()
+
 	server := webserver.NewWebServer(":8081")
 
 	server.MountMiddlewares()
@@ -24,5 +47,17 @@ func main() {
 	controller := controllers.NewController(server.Router, handler)
 	controller.Route()
 
-	server.Start()
+	go func() {
+		server.Start()
+	}()
+
+	select {
+	case <-signChannel:
+		log.Println("shutting down server gracefully...")
+	case <-ctx.Done():
+		log.Println("shutting down server...")
+	}
+
+	_, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
 }
