45962ca0591cca7856ad379d271f122211ba842a
Victor Kamei Kay <kameikay@gmail.com>
2024-03-05 16:12:26 -0300

    [service-input] add otel init on main

--- service-input/cmd/server.go
+++ service-input/cmd/server.go
@@ -1,6 +1,12 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+
 	"github.com/kameikay/service-input/configs"
 	"github.com/kameikay/service-input/internal/infra/web/controllers"
 	"github.com/kameikay/service-input/internal/infra/web/handlers"
@@ -14,8 +20,24 @@ func main() {
 		panic(err)
 	}
 
-	server := webserver.NewWebServer(":8080")
+	signChannel := make(chan os.Signal, 1)
+	signal.Notify(signChannel, os.Interrupt)
 
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	shutdown, err := configs.SetupOTel(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer func() {
+		if err := shutdown(ctx); err != nil {
+			log.Fatal("failed to shutdown tracer provider: ", err)
+		}
+	}()
+
+	server := webserver.NewWebServer(":8080")
 	server.MountMiddlewares()
 
 	apiService := service.NewGetTemperatureService()
@@ -23,5 +45,17 @@ func main() {
 	controller := controllers.NewController(server.Router, handler)
 	controller.Route()
 
-	server.Start()
+	go func() {
+		server.Start()
+	}()
+
+	select {
+	case <-signChannel:
+		log.Println("shutting down server gracefully...")
+	case <-ctx.Done():
+		log.Println("shutting down server...")
+	}
+
+	_, shutdownCancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer shutdownCancel()
 }
